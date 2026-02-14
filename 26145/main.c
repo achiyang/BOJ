4dd191faf2abc86dc24565c3e515df10f24ff4b4
achiyang <sinhouse2@gmail.com>
2026-02-14 23:57:47 +0900

    ^Q^~

--- 26145/main.c
+++ 26145/main.c
@@ -1,23 +1,30 @@
 #include <stdio.h>
 
+#define MAX 2000
+
 int main() {
-    int N, M, arr[2000] = { 0 };
+    // N: 출제자 수, M: 검수자 수, arr: 각 인원의 잔고 (최대 2000명)
+    int N, M, arr[MAX] = { 0 };
 
     scanf("%d %d", &N, &M);
 
+    // 출제자들의 초기 자본
     for (int i = 0; i < N; i++) {
         scanf("%d", &arr[i]);
     }
 
+    // 자금 분배 (N x (N + M))
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N + M; j++) {
             int T;
             scanf("%d", &T);
-            arr[i] -= T;
-            arr[j] += T;
+
+            arr[i] -= T;  // 주는 사람의 잔고 차감
+            arr[j] += T;  // 받는 사람의 잔고 증가
         }
     }
 
+    // 결과 출력
     for (int i = 0; i < N + M; i++) {
         printf("%d ", arr[i]);
     }
